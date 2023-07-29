152c201fc8aefd587d574f174e78b488bce34265
Tejas Raghuram <tejas.raghuram101@gmail.com>
2023-07-29 14:42:13 -0400

    Made Teamless Main Activity

--- app/src/main/java/com/example/scorts/Team.java
+++ app/src/main/java/com/example/scorts/Team.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Teamless extends AppCompatActivity {
+public class Team extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teamless);
+        setContentView(R.layout.activity_team);
     }
 }
